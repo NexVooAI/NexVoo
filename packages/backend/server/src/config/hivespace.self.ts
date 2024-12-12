e3e19a2c6183cfa4b1695c5899036127e131dece
NexVoo <nexxcall1@gmail.com>
2024-12-12 15:49:48 +0300

    NexVoo

--- packages/backend/server/src/config/hivespace.self.ts
+++ packages/backend/server/src/config/hivespace.self.ts
@@ -1,54 +1,54 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-// Custom configurations for HiveSpace Cloud
+// Custom configurations for NexVoo Cloud
 // ====================================================================================
 // Q: WHY THIS FILE EXISTS?
-// A: HiveSpace deployment environment may have a lot of custom environment variables,
-//    which are not suitable to be put in the `HiveSpace.ts` file.
-//    For example, HiveSpace Cloud Clusters are deployed on Google Cloud Platform.
+// A: NexVoo deployment environment may have a lot of custom environment variables,
+//    which are not suitable to be put in the `NexVoo.ts` file.
+//    For example, NexVoo Cloud Clusters are deployed on Google Cloud Platform.
 //    We need to enable the `gcloud` plugin to make sure the nodes working well,
 //    but the default selfhost version may not require it.
-//    So it's not a good idea to put such logic in the common `HiveSpace.ts` file.
+//    So it's not a good idea to put such logic in the common `NexVoo.ts` file.
 //
 //    ```
-//    if (HiveSpace.deploy) {
-//      HiveSpace.plugins.use('gcloud');
+//    if (NexVoo.deploy) {
+//      NexVoo.plugins.use('gcloud');
 //    }
 //    ```
 // ====================================================================================
 const env = process.env;
 
-HiveSpace.metrics.enabled = !HiveSpace.node.test;
+NexVoo.metrics.enabled = !NexVoo.node.test;
 
 if (env.R2_OBJECT_STORAGE_ACCOUNT_ID) {
-  HiveSpace.plugins.use('cloudflare-r2', {
+  NexVoo.plugins.use('cloudflare-r2', {
     accountId: env.R2_OBJECT_STORAGE_ACCOUNT_ID,
     credentials: {
       accessKeyId: env.R2_OBJECT_STORAGE_ACCESS_KEY_ID!,
       secretAccessKey: env.R2_OBJECT_STORAGE_SECRET_ACCESS_KEY!,
     },
   });
-  HiveSpace.storage.storages.avatar.provider = 'cloudflare-r2';
-  HiveSpace.storage.storages.avatar.bucket = 'account-avatar';
-  HiveSpace.storage.storages.avatar.publicLinkFactory = key =>
-    `https://avatar.HiveSpaceassets.com/${key}`;
+  NexVoo.storage.storages.avatar.provider = 'cloudflare-r2';
+  NexVoo.storage.storages.avatar.bucket = 'account-avatar';
+  NexVoo.storage.storages.avatar.publicLinkFactory = key =>
+    `https://avatar.NexVooassets.com/${key}`;
 
-  HiveSpace.storage.storages.blob.provider = 'cloudflare-r2';
-  HiveSpace.storage.storages.blob.bucket = `workspace-blobs-${
-    HiveSpace.HiveSpace.canary ? 'canary' : 'prod'
+  NexVoo.storage.storages.blob.provider = 'cloudflare-r2';
+  NexVoo.storage.storages.blob.bucket = `workspace-blobs-${
+    NexVoo.NexVoo.canary ? 'canary' : 'prod'
   }`;
 
-  HiveSpace.storage.storages.copilot.provider = 'cloudflare-r2';
-  HiveSpace.storage.storages.copilot.bucket = `workspace-copilot-${
-    HiveSpace.HiveSpace.canary ? 'canary' : 'prod'
+  NexVoo.storage.storages.copilot.provider = 'cloudflare-r2';
+  NexVoo.storage.storages.copilot.bucket = `workspace-copilot-${
+    NexVoo.NexVoo.canary ? 'canary' : 'prod'
   }`;
 }
 
-HiveSpace.plugins.use('copilot', {
+NexVoo.plugins.use('copilot', {
   openai: {},
   fal: {},
 });
-HiveSpace.plugins.use('redis');
-HiveSpace.plugins.use('payment', {
+NexVoo.plugins.use('redis');
+NexVoo.plugins.use('payment', {
   stripe: {
     keys: {
       // fake the key to ensure the server generate full GraphQL Schema even env vars are not set
@@ -57,10 +57,10 @@ HiveSpace.plugins.use('payment', {
     },
   },
 });
-HiveSpace.plugins.use('oauth');
+NexVoo.plugins.use('oauth');
 
-if (HiveSpace.deploy) {
-  HiveSpace.mailer = {
+if (NexVoo.deploy) {
+  NexVoo.mailer = {
     service: 'gmail',
     auth: {
       user: env.MAILER_USER,
@@ -68,5 +68,5 @@ if (HiveSpace.deploy) {
     },
   };
 
-  HiveSpace.plugins.use('gcloud');
+  NexVoo.plugins.use('gcloud');
 }
